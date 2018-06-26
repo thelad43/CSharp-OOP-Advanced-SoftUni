5308e02ae8f55609e136ae4b81c94635d08e5356
Teodor <thelad43@gmail.com>
2018-06-26 15:32:55 +0300

    Added solution for problem 01. Box

---
+++ 03.  Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Box
+{
+    using System.Collections.Generic;
+
+    public class Box<T>
+    {
+        private readonly List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public int Count => this.data.Count;
+
+        public void Add(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Remove()
+        {
+            var elementForRemoving = this.data[this.data.Count - 1];
+            this.data.RemoveAt(this.data.Count - 1);
+            return elementForRemoving;
+        }
+    }
+}
