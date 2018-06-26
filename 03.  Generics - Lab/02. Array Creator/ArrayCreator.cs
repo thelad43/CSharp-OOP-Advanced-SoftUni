ff3785edfe211c473937a35cd5327f11cddb8133
Teodor <thelad43@gmail.com>
2018-06-26 15:34:02 +0300

    Added solution for problem 02. Array Creator

---
+++ 03.  Generics - Lab/02. Array Creator/ArrayCreator.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Array_Creator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            var items = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                items[i] = item;
+            }
+
+            return items;
+        }
+    }
+}
---
+++ 03.  Generics - Lab/02. Array Creator/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _02._Array_Creator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var strings = ArrayCreator.Create(5, "Pesho");
+            var integers = ArrayCreator.Create(10, 33);
+
+            Console.WriteLine(string.Join(" ", strings));
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
