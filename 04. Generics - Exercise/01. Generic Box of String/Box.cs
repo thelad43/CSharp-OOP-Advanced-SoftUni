20834acca398f0d447af3283264aeebda4b4d7f2
Teodor <thelad43@gmail.com>
2018-06-27 12:05:58 +0300

    Added solution for problem 01. Generic Box of String

---
+++ 04. Generics - Exercise/01. Generic Box of String/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace _01._Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        private readonly T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType()}: {this.value}";
+        }
+    }
+}
---
+++ 04. Generics - Exercise/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _01._Generic_Box_of_String
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentBox = new Box<string>(Console.ReadLine());
+                Console.WriteLine(currentBox);
+            }
+        }
+    }
+}
