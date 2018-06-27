815b72ed17ff29b250b523493514745368e08166
Teodor <thelad43@gmail.com>
2018-06-27 12:19:37 +0300

    Added solution for problem 08. Custom List Sorter

---
+++ 04. Generics - Exercise/08. Custom List Sorter/Interfaces/ICustomList.cs
@@ -0,0 +1,23 @@
+﻿namespace _08._Custom_List_Sorter.Interfaces
+{
+    public interface ICustomList<T>
+    {
+        void Add(T element);
+
+        T Remove(int index);
+
+        bool Contains(T item);
+
+        void Swap(int index1, int index2);
+
+        void Sort();
+
+        int CountGreaterThan(T element);
+
+        void PrintItems();
+
+        T Max();
+
+        T Min();
+    }
+}
---
+++ 04. Generics - Exercise/08. Custom List Sorter/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _08._Custom_List_Sorter
+{
+    using Controllers;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
