8e5800480a075b27ed0009e6ee975950afb71b28
Teodor <thelad43@gmail.com>
2018-06-27 12:09:37 +0300

    Added solution for problem 04. Generic Swap Method Integer

---
+++ 04. Generics - Exercise/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,22 @@
+﻿namespace _04._Generic_Swap_Method_Integer
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.Value = default(T);
+        }
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
---
+++ 04. Generics - Exercise/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _04._Generic_Swap_Method_Integer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var boxes = new List<Box<int>>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentBox = new Box<int>(int.Parse(Console.ReadLine()));
+                boxes.Add(currentBox);
+            }
+
+            var swapIndices = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(boxes, swapIndices[0], swapIndices[1]);
+            Console.WriteLine(string.Join(Environment.NewLine, boxes));
+        }
+
+        private static void Swap<T>(IList<T> collection, int firstSwapIndex, int secondSwapIndex)
+        {
+            var oldValue = collection[firstSwapIndex];
+            collection[firstSwapIndex] = collection[secondSwapIndex];
+            collection[secondSwapIndex] = oldValue;
+        }
+    }
+}
