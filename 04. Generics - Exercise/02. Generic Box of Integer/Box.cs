2a93b5595f6b380a1893ca450374a9a5e73c39ca
Teodor <thelad43@gmail.com>
2018-06-27 12:07:21 +0300

    Added solution for problem 02. Generic Box of Integer

---
+++ 04. Generics - Exercise/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Generic_Box_of_Integer
+{
+    public class Box<T>
+    {
+        private readonly T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType()}: {this.value}";
+        }
+    }
+}
---
+++ 04. Generics - Exercise/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Generic_Box_of_Integer
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentBox = new Box<int>(int.Parse(Console.ReadLine()));
+                Console.WriteLine(currentBox);
+            }
+        }
+    }
+}
