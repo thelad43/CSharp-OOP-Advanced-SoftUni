9408c3a9cf07514426870e16e0736b97ef667c61
Teodor <thelad43@gmail.com>
2018-06-27 12:12:56 +0300

    Added solution for problem 06. Generic Count Method Double

---
+++ 04. Generics - Exercise/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,30 @@
+﻿namespace _06._Generic_Count_Method_Double
+{
+    using System;
+
+    public class Box<T> : IComparable<Box<T>>
+        where T : IComparable
+    {
+        public Box()
+        {
+            this.Value = default(T);
+        }
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public int CompareTo(Box<T> other)
+        {
+            return this.Value.CompareTo(other.Value);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
---
+++ 04. Generics - Exercise/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _06._Generic_Count_Method_Double
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var boxes = new List<Box<double>>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                boxes.Add(new Box<double>(double.Parse(Console.ReadLine())));
+            }
+
+            var comparison = new Box<double>(double.Parse(Console.ReadLine()));
+            Console.WriteLine(GetCountGreaterThanComparisonValue(boxes, comparison));
+        }
+
+        private static int GetCountGreaterThanComparisonValue<T>(IEnumerable<T> collection, T comparison)
+            where T : IComparable<T>
+        {
+            var count = 0;
+
+            foreach (var element in collection)
+            {
+                if (element.CompareTo(comparison) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
