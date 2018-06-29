abfdbfcd8e95017a7abf2ade9730dfd25d6283d2
Teodor <thelad43@gmail.com>
2018-06-29 13:14:34 +0300

    Added solution for problem 03. ComparableBook

---
+++ 05. Iterators and Comparators - Lab/03. ComparableBook/Library.cs
@@ -0,0 +1,25 @@
+﻿namespace _03._ComparableBook
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = new SortedSet<Book>(books);
+        }
+
+        public SortedSet<Book> Books { get; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return this.Books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
