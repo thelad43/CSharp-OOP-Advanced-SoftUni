b97b3d831eeb688f6eba5511a04662f262fb8209
Teodor <thelad43@gmail.com>
2018-06-29 13:13:19 +0300

    Added solution for problem 02. LibraryIterator

---
+++ 05. Iterators and Comparators - Lab/02. LibraryIterator/Library.cs
@@ -0,0 +1,56 @@
+﻿namespace _02._LibraryIterator
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = books;
+        }
+
+        public IReadOnlyList<Book> Books { get; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            // return this.Books.GetEnumerator();
+            return new LibraryIterator(this.Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[this.currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return ++this.currentIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.currentIndex = -1;
+            }
+        }
+    }
+}
