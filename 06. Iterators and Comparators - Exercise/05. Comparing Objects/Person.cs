1aaaf53595d9ed6515038bb85832c679bcd38b82
Teodor <thelad43@gmail.com>
2018-07-01 14:10:02 +0300

    Added solution for problem 05. Comparing Objects

---
+++ 06. Iterators and Comparators - Exercise/05. Comparing Objects/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace _05._Comparing_Objects
+{
+    using System;
+
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            var comparison = this.Name.CompareTo(other.Name);
+
+            if (comparison == 0)
+            {
+                comparison = this.Age.CompareTo(other.Age);
+            }
+
+            if (comparison == 0)
+            {
+                comparison = this.Town.CompareTo(other.Town);
+            }
+
+            return comparison;
+        }
+    }
+}
---
+++ 06. Iterators and Comparators - Exercise/05. Comparing Objects/StartUp.cs
@@ -0,0 +1,71 @@
+﻿namespace _05._Comparing_Objects
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        private static List<Person> people;
+
+        public static void Main()
+        {
+            people = ReadPeople();
+            var n = int.Parse(Console.ReadLine());
+            var wantedPerson = people[n - 1];
+
+            var equalPeople = 0;
+            var differentPeople = 0;
+
+            foreach (var person in people)
+            {
+                if (person.CompareTo(wantedPerson) == 0)
+                {
+                    equalPeople++;
+                }
+                else
+                {
+                    differentPeople++;
+                }
+            }
+
+            PrintResult(equalPeople, differentPeople);
+        }
+
+        private static void PrintResult(int equalPeople, int differentPeople)
+        {
+            if (equalPeople > 1)
+            {
+                Console.Write(equalPeople + " ");
+                Console.Write(differentPeople + " ");
+                Console.WriteLine(equalPeople + differentPeople);
+                return;
+            }
+
+            Console.WriteLine("No matches");
+        }
+
+        private static List<Person> ReadPeople()
+        {
+            var people = new List<Person>();
+
+            while (true)
+            {
+                var inputTokens = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputTokens[0] == "END")
+                {
+                    break;
+                }
+
+                var name = inputTokens[0];
+                var age = int.Parse(inputTokens[1]);
+                var town = inputTokens[2];
+
+                people.Add(new Person(name, age, town));
+            }
+
+            return people;
+        }
+    }
+}
