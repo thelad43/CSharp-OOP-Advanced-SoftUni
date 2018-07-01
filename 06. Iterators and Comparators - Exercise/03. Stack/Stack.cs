0d3b07a296f031b0f33f3ea45a7cef9b3df74443
Teodor <thelad43@gmail.com>
2018-07-01 14:07:21 +0300

    Added solution for problem 03. Stack

---
+++ 06. Iterators and Comparators - Exercise/03. Stack/Stack.cs
@@ -0,0 +1,47 @@
+﻿namespace _03._Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Stack<T> : IEnumerable<T>
+    {
+        private readonly IList<T> data;
+
+        public Stack(IEnumerable<T> elements)
+        {
+            this.data = new List<T>(elements);
+        }
+
+        public void Push(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Pop()
+        {
+            if (!this.data.Any())
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            var poppedElement = this.data.LastOrDefault();
+            this.data.Remove(poppedElement);
+            return poppedElement;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.data.Count - 1; i >= 0; i--)
+            {
+                yield return this.data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 06. Iterators and Comparators - Exercise/03. Stack/StartUp.cs
@@ -0,0 +1,70 @@
+﻿namespace _03._Stack
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Stack<int> stack = null;
+
+            while (true)
+            {
+                var inputTokens = Console.ReadLine()
+                    .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputTokens[0] == "END")
+                {
+                    break;
+                }
+
+                try
+                {
+                    stack = ProcessCommand(stack, inputTokens);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+        }
+
+        private static Stack<int> ProcessCommand(Stack<int> stack, string[] inputTokens)
+        {
+            switch (inputTokens[0])
+            {
+                case "Push":
+                    var elements = inputTokens.Skip(1)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    if (stack == null)
+                    {
+                        stack = new Stack<int>(elements);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < elements.Length; i++)
+                        {
+                            stack.Push(elements[i]);
+                        }
+                    }
+
+                    break;
+
+                case "Pop":
+                    stack.Pop();
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
+            return stack;
+        }
+    }
+}
