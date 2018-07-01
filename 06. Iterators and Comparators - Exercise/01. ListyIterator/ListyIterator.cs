03148a72e77658d07d7db8a7b2c84531c7162b37
Teodor <thelad43@gmail.com>
2018-07-01 14:04:41 +0300

    Added solution for problem 01. ListyIterator

---
+++ 06. Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,42 @@
+﻿namespace _01._ListyIterator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ListyIterator<T>
+    {
+        private readonly IList<T> elements;
+        private int currentIndex;
+
+        public ListyIterator(IEnumerable<T> elements)
+        {
+            this.elements = new List<T>(elements);
+        }
+
+        public bool Move()
+        {
+            return ++this.currentIndex < this.elements.Count;
+        }
+
+        public bool HasNext()
+        {
+            return this.currentIndex < this.elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (!this.elements.Any())
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            if (this.currentIndex > this.elements.Count - 1)
+            {
+                this.currentIndex = this.elements.Count - 1;
+            }
+
+            Console.WriteLine(this.elements[this.currentIndex]);
+        }
+    }
+}
