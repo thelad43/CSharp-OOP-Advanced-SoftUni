807fefe65a91c517bc482005a117dda94746c568
Teodor <thelad43@gmail.com>
2018-07-01 14:15:57 +0300

    Added solution for problem 07. Equality Logic

---
+++ 06. Iterators and Comparators - Exercise/07. Equality Logic/Comparators/PersonEqualityComparator.cs
@@ -0,0 +1,17 @@
+﻿namespace _07._Equality_Logic.Comparators
+{
+    using System.Collections.Generic;
+
+    public class PersonEqualityComparator : IEqualityComparer<Person>
+    {
+        public bool Equals(Person x, Person y)
+        {
+            return x.CompareTo(y) == 0;
+        }
+
+        public int GetHashCode(Person person)
+        {
+            return $"{person.Name} {person.Age}".GetHashCode();
+        }
+    }
+}
---
+++ 06. Iterators and Comparators - Exercise/07. Equality Logic/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace _07._Equality_Logic
+{
+    using Comparators;
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        private static SortedSet<Person> sortedSetPeople;
+        private static HashSet<Person> hashSetPeople;
+
+        public static void Main()
+        {
+            sortedSetPeople = new SortedSet<Person>();
+            hashSetPeople = new HashSet<Person>(new PersonEqualityComparator());
+            FillSets();
+            Console.WriteLine(sortedSetPeople.Count);
+            Console.WriteLine(hashSetPeople.Count);
+        }
+
+        private static void FillSets()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var personInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var name = personInfo[0];
+                var age = int.Parse(personInfo[1]);
+                var person = new Person(name, age);
+
+                sortedSetPeople.Add(person);
+                hashSetPeople.Add(person);
+            }
+        }
+    }
+}
