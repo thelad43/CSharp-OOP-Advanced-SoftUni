977d455addeb7bfb3ffa5b868c4074b8037c1e48
Teodor <thelad43@gmail.com>
2018-07-01 14:05:58 +0300

    Added solution for problem 02. Collection

---
+++ 06. Iterators and Comparators - Exercise/02. Collection/ListyIterator.cs
@@ -0,0 +1,56 @@
+﻿namespace _02._Collection
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private readonly IList<T> elements;
+        private int currentIndex;
+
+        public ListyIterator(IEnumerable<T> elements)
+        {
+            this.elements = new List<T>(elements);
+        }
+
+        public bool Move()
+        {
+            return ++this.currentIndex < this.elements.Count;
+        }
+
+        public bool HasNext()
+        {
+            return this.currentIndex < this.elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (!this.elements.Any())
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            if (this.currentIndex > this.elements.Count - 1)
+            {
+                this.currentIndex = this.elements.Count - 1;
+            }
+
+            Console.WriteLine(this.elements[this.currentIndex]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.elements.Count; i++)
+            {
+                yield return this.elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 06. Iterators and Comparators - Exercise/02. Collection/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace _02._Collection
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ListyIterator<string> listyIterator = null;
+
+            while (true)
+            {
+                var inputTokens = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputTokens[0] == "END")
+                {
+                    break;
+                }
+
+                try
+                {
+                    listyIterator = ProcessCommand(listyIterator, inputTokens);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static ListyIterator<string> ProcessCommand(ListyIterator<string> listyIterator, string[] inputTokens)
+        {
+            switch (inputTokens[0])
+            {
+                case "Create":
+                    var elements = inputTokens.Skip(1).ToArray();
+                    listyIterator = new ListyIterator<string>(elements);
+                    break;
+
+                case "Move":
+                    Console.WriteLine(listyIterator.Move());
+                    break;
+
+                case "Print":
+                    listyIterator.Print();
+                    break;
+
+                case "PrintAll":
+                    Console.WriteLine(string.Join(" ", listyIterator));
+                    break;
+
+                case "HasNext":
+                    Console.WriteLine(listyIterator.HasNext());
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
+            return listyIterator;
+        }
+    }
+}
