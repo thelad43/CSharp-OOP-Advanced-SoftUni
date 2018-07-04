89afe2a8a44e5f802e8fe14903827c6ecd23fa5a
Teodor <thelad43@gmail.com>
2018-07-04 12:49:25 +0300

    Added solution for problem 05. Create Attribute

---
+++ 07. Reflection and Attributes - Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace _05._Create_Attribute
+{
+    [SoftUni("Ventsi")]
+    public class StartUp
+    {
+        [SoftUni("Gosho")]
+        public static void Main()
+        {
+        }
+    }
+}
