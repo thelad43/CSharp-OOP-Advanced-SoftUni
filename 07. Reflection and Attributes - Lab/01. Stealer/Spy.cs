f22bc467fca29f0260678e8233cd4bd0db96b417
Teodor <thelad43@gmail.com>
2018-07-04 12:42:58 +0300

    Added solution for problem 01. Stealer

---
+++ 07. Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            var type = Type.GetType(GetType().Namespace + "." + className);
+
+            var classFields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic |
+                BindingFlags.Public | BindingFlags.Static);
+
+            var result = new StringBuilder();
+            var classInstance = Activator.CreateInstance(type, new object[] { });
+            result.AppendLine($"Class under investigation: {className}");
+
+            foreach (var field in classFields.Where(f => fields.Contains(f.Name)))
+            {
+                result.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
---
+++ 07. Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _01._Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var spy = new Spy();
+            var result = spy.StealFieldInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
