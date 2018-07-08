6cc0c02132176769aad7379c4213d80d7eaa23a7
Teodor <thelad43@gmail.com>
2018-07-08 15:02:06 +0300

    Added solution for problem 01. Harvesting Fields

---
+++ 08. Reflection and Attributes - Exercise/01. Harvesting Fields/Engine.cs
@@ -0,0 +1,84 @@
+﻿namespace _01._Harvesting_Fields
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Engine
+    {
+        private readonly StringBuilder stringBuilder;
+
+        public Engine()
+        {
+            this.stringBuilder = new StringBuilder();
+        }
+
+        public void Run()
+        {
+            ProcessCommands();
+
+            PrintResult();
+        }
+
+        private void ProcessCommands()
+        {
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+                var type = typeof(HarvestingFields);
+                var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+                if (inputLine == "HARVEST")
+                {
+                    break;
+                }
+
+                switch (inputLine)
+                {
+                    case "private":
+                        this.AppendFieldsToStringBuilder(fields.Where(f => f.IsPrivate));
+                        break;
+
+                    case "protected":
+                        this.AppendFieldsToStringBuilder(fields.Where(f => f.IsFamily));
+                        break;
+
+                    case "public":
+                        this.AppendFieldsToStringBuilder(fields.Where(f => f.IsPublic));
+                        break;
+
+                    case "all":
+                        this.AppendFieldsToStringBuilder(fields);
+                        break;
+
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+        }
+
+        private void PrintResult()
+        {
+            Console.WriteLine(this.stringBuilder.ToString().Trim());
+        }
+
+        private void AppendFieldsToStringBuilder(IEnumerable<FieldInfo> fields)
+        {
+            var accessModifier = string.Empty;
+
+            foreach (var field in fields)
+            {
+                accessModifier = field.Attributes.ToString().ToLower();
+
+                if (accessModifier.CompareTo("family") == 0)
+                {
+                    accessModifier = "protected";
+                }
+
+                this.stringBuilder.AppendLine($"{accessModifier} {field.FieldType.Name} {field.Name}");
+            }
+        }
+    }
+}
---
+++ 08. Reflection and Attributes - Exercise/01. Harvesting Fields/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace _01._Harvesting_Fields
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
