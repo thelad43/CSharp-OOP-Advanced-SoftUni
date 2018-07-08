1f8b8849ae40b3b6e61d9b0167f9b5c95dade3a6
Teodor <thelad43@gmail.com>
2018-07-08 15:27:18 +0300

    Added solution for problem 06. Traffic Lights

---
+++ 08. Reflection and Attributes - Exercise/06. Traffic Lights/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _06._Traffic_Lights
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var trafficLights = new List<TrafficLight>();
+
+            var inputTokens = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < inputTokens.Length; i++)
+            {
+                trafficLights.Add((TrafficLight)Activator.CreateInstance(typeof(TrafficLight), new[] { inputTokens[i] }));
+            }
+
+            var n = int.Parse(Console.ReadLine());
+
+            var stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                foreach (var trafficLight in trafficLights)
+                {
+                    trafficLight.UpdateSignal();
+                    var field = typeof(TrafficLight).GetField("signal", BindingFlags.Instance | BindingFlags.NonPublic);
+                    stringBuilder.Append(field.GetValue(trafficLight) + " ");
+                }
+
+                stringBuilder.AppendLine();
+            }
+
+            Console.WriteLine(stringBuilder.ToString().Trim());
+        }
+    }
+}
---
+++ 08. Reflection and Attributes - Exercise/06. Traffic Lights/TrafficLight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06._Traffic_Lights
+{
+    public class TrafficLight
+    {
+        private const int NumberOfSignals = 3;
+
+        private Signal signal;
+
+        public TrafficLight(string signal)
+        {
+            this.signal = (Signal)Enum.Parse(typeof(Signal), signal);
+        }
+
+        public void UpdateSignal()
+        {
+            this.signal =
+                (Signal)Enum.Parse(typeof(Signal), (((int)(this.signal) + 1) % NumberOfSignals).ToString());
+        }
+    }
+}
