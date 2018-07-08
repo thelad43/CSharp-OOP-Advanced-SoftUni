32cdce5dec70df69df6d23b1927f4b5adbaf0fe3
Teodor <thelad43@gmail.com>
2018-07-08 15:03:25 +0300

    Added solution for problem  02. Black Box Integer

---
+++ 08. Reflection and Attributes - Exercise/02. Black Box Integer/Engine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace _02._Black_Box_Integer
+{
+    public class Engine
+    {
+        private readonly StringBuilder stringBuilder;
+
+        public Engine()
+        {
+            this.stringBuilder = new StringBuilder();
+        }
+
+        public void Run()
+        {
+            var type = typeof(BlackBoxInteger);
+            var classInstance = Activator.CreateInstance(type, true);
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var result = ProcessCommands(classInstance, methods, fields);
+            Console.WriteLine(result);
+        }
+
+        private string ProcessCommands(object classInstance, MethodInfo[] methods, FieldInfo[] fields)
+        {
+            while (true)
+            {
+                var inputTokens = Console.ReadLine().Split('_', StringSplitOptions.RemoveEmptyEntries);
+                var command = inputTokens[0];
+
+                if (inputTokens[0] == "END")
+                {
+                    break;
+                }
+
+                var number = int.Parse(inputTokens[1]);
+
+                methods
+                    .FirstOrDefault(m => m.Name == command)
+                    .Invoke(classInstance, new object[] { number });
+
+                foreach (var field in fields)
+                {
+                    this.stringBuilder.AppendLine(field.GetValue(classInstance).ToString());
+                }
+            }
+
+            var result = this.stringBuilder.ToString().Trim();
+            return result;
+        }
+    }
+}
---
+++ 08. Reflection and Attributes - Exercise/02. Black Box Integer/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace _02._Black_Box_Integer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
