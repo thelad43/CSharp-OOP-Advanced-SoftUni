dcb87ba12cdd1b60a1f9770f831b2b2375c31bf3
Teodor <thelad43@gmail.com>
2018-07-14 11:48:37 +0300

    Added solution for problem 03. Iterator Test

---
+++ 10. Unit Testing - Exercise/03. Iterator Test/ListIterator.cs
@@ -0,0 +1,51 @@
+﻿namespace _03._Iterator_Test
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ListIterator
+    {
+        private readonly IList<string> data;
+        private int currentIndex;
+
+        public ListIterator(IList<string> data)
+        {
+            this.CheckNullData(data);
+            this.data = data;
+        }
+
+        public bool Move()
+        {
+            if (this.currentIndex < this.data.Count - 1)
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return this.currentIndex < this.data.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (this.data == null || this.data.Count == 0)
+            {
+                throw new InvalidOperationException("The collection is empty!");
+            }
+
+            Console.WriteLine(this.data[this.currentIndex]);
+        }
+
+        private void CheckNullData(IList<string> data)
+        {
+            if (data == null || data.Count == 0)
+            {
+                throw new ArgumentNullException("data", "The data cannot be null!");
+            }
+        }
+    }
+}
---
+++ 10. Unit Testing - Exercise/03. Iterator Test/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace _03._Iterator_Test
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ListIterator listIterator = null;
+
+            try
+            {
+                listIterator = ProcessCommands(listIterator);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static ListIterator ProcessCommands(ListIterator listIterator)
+        {
+            while (true)
+            {
+                var commandTokens = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (commandTokens[0] == "END")
+                {
+                    break;
+                }
+
+                switch (commandTokens[0])
+                {
+                    case "Create":
+                        if (commandTokens.Length == 1)
+                        {
+                            throw new InvalidOperationException("Invalid Operation!");
+                        }
+
+                        listIterator = new ListIterator(commandTokens.Skip(1).ToList());
+                        break;
+
+                    case "Print":
+                        listIterator.Print();
+                        break;
+
+                    case "Move":
+                        Console.WriteLine(listIterator.Move());
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(listIterator.HasNext());
+                        break;
+
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+
+            return listIterator;
+        }
+    }
+}
