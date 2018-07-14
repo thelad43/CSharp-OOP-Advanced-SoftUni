c009e7246e816c9441f24b399dd2c00883705cda
Teodor <thelad43@gmail.com>
2018-07-14 11:45:34 +0300

    Added solution for problem 01. Database

---
+++ 10. Unit Testing - Exercise/01. Database/Database.cs
@@ -0,0 +1,71 @@
+﻿namespace _01._Database
+{
+    using System;
+
+    public class Database
+    {
+        private const int Capacty = 16;
+        private const int MaxIndex = 16;
+
+        private readonly int[] data;
+        private int currentIndex = 0;
+
+        public Database()
+        {
+            this.data = new int[Capacty];
+        }
+
+        public Database(params int[] items)
+        {
+            this.CheckCountItems(items);
+            this.data = new int[Capacty];
+            this.FillData(items);
+        }
+
+        private void FillData(int[] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                this.data[this.currentIndex] = items[i];
+                this.currentIndex++;
+            }
+        }
+
+        private void CheckCountItems(int[] items)
+        {
+            if (items.Length > 16)
+            {
+                throw new InvalidOperationException("Items cannot be more than 16!");
+            }
+        }
+
+        public void Add(int item)
+        {
+            if (this.currentIndex == MaxIndex)
+            {
+                throw new InvalidOperationException("The Database is full!");
+            }
+
+            this.data[this.currentIndex] = item;
+            this.currentIndex++;
+        }
+
+        public void Remove()
+        {
+            if (this.currentIndex == 0)
+            {
+                throw new InvalidOperationException("The Database if empty!");
+            }
+
+            this.data[this.currentIndex] = 0;
+            this.currentIndex--;
+        }
+
+        public int[] Fetch()
+        {
+            var result = new int[this.currentIndex];
+            Array.Copy(this.data, result, this.currentIndex);
+            return result;
+        }
+    }
+}
---
+++ 10. Unit Testing - Exercise/01. Database/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _01._Database
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new Database(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            db.Add(11);
+            db.Add(12);
+            db.Add(13);
+
+            db.Remove();
+            db.Remove();
+
+            var result = db.Fetch();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
