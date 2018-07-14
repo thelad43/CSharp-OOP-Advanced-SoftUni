c64fe6a7739a9396975e59c07fa7f62d3287344c
Teodor <thelad43@gmail.com>
2018-07-14 11:49:52 +0300

    Added solution for problem 04. Bubble Sort Test

---
+++ 10. Unit Testing - Exercise/04. Bubble Sort Test/Bubble.cs
@@ -0,0 +1,50 @@
+﻿namespace _04._Bubble_Sort_Test
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Bubble<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        public Bubble(IList<T> items)
+        {
+            this.Data = items;
+            this.SortItems();
+        }
+
+        public IList<T> Data { get; }
+
+        public void Add(T item)
+        {
+            this.Data.Add(item);
+            this.SortItems();
+        }
+
+        private void SortItems()
+        {
+            for (int i = 0; i < this.Data.Count; i++)
+            {
+                for (int k = 0; k < this.Data.Count - 1; k++)
+                {
+                    if (this.Data[k].CompareTo(this.Data[k + 1]) > 0)
+                    {
+                        var oldValue = this.Data[k + 1];
+                        this.Data[k + 1] = this.Data[k];
+                        this.Data[k] = oldValue;
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.Data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 10. Unit Testing - Exercise/04. Bubble Sort Test/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _04._Bubble_Sort_Test
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = new List<int>
+            {
+                76,
+                74,
+                18,
+                24,
+                17,
+                57,
+                55,
+                42,
+                53,
+                43,
+                67,
+                45,
+                78,
+                14,
+                40,
+            };
+
+            var bubble = new Bubble<int>(numbers);
+            Console.WriteLine(string.Join(", ", bubble));
+        }
+    }
+}
