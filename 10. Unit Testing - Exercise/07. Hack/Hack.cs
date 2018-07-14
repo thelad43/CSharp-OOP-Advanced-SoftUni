75e65c131d8a83b42a5bfa91e9c16605fc4398f9
Teodor <thelad43@gmail.com>
2018-07-14 11:58:46 +0300

    Added solution for problem 07. Hack

---
+++ 10. Unit Testing - Exercise/07. Hack/Hack.cs
@@ -0,0 +1,13 @@
+﻿namespace _07._Hack
+{
+    using System;
+
+    public class Hack
+    {
+        public static void Main()
+        {
+            Math.Abs(-5.2);
+            Math.Floor(2.98);
+        }
+    }
+}
